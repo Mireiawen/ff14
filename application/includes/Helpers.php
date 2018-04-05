8a8a8102ed7cc55d31f96c8db9dfb3e1e2c2ee56
Mireiawen Rose <mir@mireiawen.net>
2018-04-06 00:48:54 +0200

    If ID is not integer, return the string instead

--- application/includes/Helpers.php
+++ application/includes/Helpers.php
@@ -3,11 +3,17 @@ class Helpers
 {
 	public static function Smarty_XIV_Icon($params, &$smarty)
 	{
-		if ((!isset($params['id'])) || (!is_numeric($params['id'])))
+		if (!is_numeric($params['id']))
 		{
-			$smarty -> trigger_error(sprintf(_('%s: missing or invalid parameter %s'), 'xiv_icon', 'id'));
+			return $params['id'];
+		}
+		
+		if (!isset($params['id']))
+		{
+			trigger_error(sprintf(_('%s: missing or invalid parameter %s'), 'xiv_icon', 'id'), E_USER_ERROR);
 			return;
 		}
+		
 		$id = intval($params['id']);
 		
 		return sprintf('https://secure.xivdb.com/img/game/%06d/%06d.png', $id - $id % 1000, $id);
